23321862219d9e11a46475309cd02fe305e0995f
Dmitry Zhogin <zhogin.dima@gmail.com>
2025-06-12 17:02:50 +0300

    Added simple fastapi endpoint

--- run_inference.py
+++ run_inference.py
@@ -1,6 +1,8 @@
 from langdetect import detect
 from langchain_core.messages import HumanMessage
 from graph import graph
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 '''
 Function to run the graph.
@@ -8,18 +10,31 @@ It detects user's language and runs the graph with user's message and detected l
 Agent only supports English and Russian.
 '''
 
-def run_messages(messages: str):
-    language = detect(messages) if detect(messages) in ['en', 'ru'] else 'en'
-    return graph.invoke({'messages': [HumanMessage(messages)], 'language':language})
+def run_inference(message: str):
+    language = detect(message) if detect(message) in ['en', 'ru'] else 'en'
+    return graph.invoke({'messages': [HumanMessage(message)], 'language':language})
 
 '''
-Test Input
+FastAPI endpoint
 '''
 
-test_input = "Как приготовить блины?"
-test_output = run_messages(test_input)
+app = FastAPI()
 
-print("_"*50)
-print(test_output)
-print("_"*50)
-print(f"Model output:\n{test_output['messages'][-1].content}\nQuery Language: {test_output['language']}")
+class ChatInput(BaseModel):
+    message: str
+
+@app.post("/chat")
+async def chat(input: ChatInput):
+    return run_inference(input.message)
+
+# '''
+# Test Input
+# '''
+
+# test_input = "Как приготовить блины?"
+# test_output = run_inference(test_input)
+
+# print("_"*50)
+# print(test_output)
+# print("_"*50)
+# print(f"Model output:\n{test_output['messages'][-1].content}\nQuery Language: {test_output['language']}")
